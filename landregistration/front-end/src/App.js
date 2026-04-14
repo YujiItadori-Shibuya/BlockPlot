1074170dae699494aa092e5bae15ec7ea61227b2
YujiItadori-Shibuya <prateekt09112004@gmail.com>
2026-04-14 12:43:43 +0530

    updated footer and react structure

--- landregistration/front-end/src/App.js
+++ landregistration/front-end/src/App.js
@@ -7,20 +7,28 @@ import FeaturesPage from './components/FeaturesPage';
 import ContactPage from './components/ContactPage';
 import ManageLandPage from './components/ManageLandPage';
 import AdminPanel from './components/AdminPanel';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <WalletProvider>
       <Router basename="/Final-Year-Project">
         <div className="app-container"> {/* Start Wrapper */}
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/features" element={<FeaturesPage />} />
-            <Route path="/manage-land" element={<ManageLandPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/admin" element={<AdminPanel />} />
-          </Routes>
+          <Navbar /> {/* Keep Navbar outside main-content if you want it fixed at top */}
+
+          <div className="main-content">
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/features" element={<FeaturesPage />} />
+              <Route path="/manage-land" element={<ManageLandPage />} />
+              <Route path="/contact" element={<ContactPage />} />
+              <Route path="/admin" element={<AdminPanel />} />
+            </Routes>
+          </div>
+
+          <Footer />
         </div> {/* End Wrapper */}
       </Router>
     </WalletProvider>
